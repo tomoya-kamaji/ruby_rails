60f0f54d3cb23acfeb5fa5e837c93a05dd486284
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-05-11 14:51:17 +0900

    test

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    # User.countが減ることが難しい
+    assert_no_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    follow_redirect!
+    assert_template 'users/new'
+  end
+end
