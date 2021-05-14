df427b8cafd04270118d36e6b5fb3db642717b2f
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-05-14 15:29:57 +0900

    ユーザ

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  # get  '/edit',  to: 'users#edit'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
